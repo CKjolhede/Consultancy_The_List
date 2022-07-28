5493d413a71cd6a86eae7ef03b9204f2544a36cb
Eric Espindola <eric_e_2012@hotmail.com>
2022-07-28 00:33:52 -0700

    Error pushing to heroku because of Gemfile. Making recommended changes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_27_035138) do
+ActiveRecord::Schema.define(version: 2022_07_27_170703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recommendations", force: :cascade do |t|
+    t.integer "media_id"
+    t.string "media_title"
+    t.integer "media_type"
+    t.bigint "referred_by_id"
+    t.bigint "referred_to_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["referred_by_id"], name: "index_recommendations_on_referred_by_id"
+    t.index ["referred_to_id"], name: "index_recommendations_on_referred_to_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -22,4 +34,6 @@ ActiveRecord::Schema.define(version: 2022_07_27_035138) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "recommendations", "users", column: "referred_by_id"
+  add_foreign_key "recommendations", "users", column: "referred_to_id"
 end
