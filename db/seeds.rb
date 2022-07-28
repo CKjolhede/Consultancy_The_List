6d974a795842a36cabd5d77ce085b00977ade422
Eric Espindola <eric_e_2012@hotmail.com>
2022-07-28 00:50:42 -0700

    Add seed data for Heroku app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
-20.times do
-  User.create!(
-    email: Faker::Internet.unique.email,
-    name: Faker::Name.name
-  )
-end
+User.create!(email: "alaine.nader@hammes.co", name: "Devon Keeling")
+User.create!(email: "rafael_leffler@gislason.biz", name: "Santiago Conn")
+User.create!(email: "jewel.schmeler@aufderhar.info", name: "Ms. Garnet Borer")
+User.create!(email: "allyson_wisozk@walter-waelchi.org", name: "Jerrell Howe")
+User.create!(email: "german_abernathy@funk.info", name: "Msgr. Shad Kihn")
+User.create!(email: "preston@crist.net", name: "Mrs. Jewell Boehm")
+User.create!(email: "owen@hartmann.co", name: "Britt Lubowitz")
+User.create!(email: "jeramy_west@wuckert-heathcote.biz", name: "Rufus Oberbrunner DVM")
+User.create!(email: "eleanor@klein.net", name: "Santana Sipes")
+User.create!(email: "brock@beatty-herman.net", name: "Sharla Torp")
