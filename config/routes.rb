d1605a0ac0f631153e5149f3f73a7af9741d792c
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-28 19:31:02 -0600

    refactor route to recomm/index to include  user namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show, :create]
-      resources :recommendations, only: [:index, :create]
+      resources :users, only: [:index, :show, :create] do
+        resources :recommendations, only: [:index, :create]
+      end
     end
   end
 end
--- spec/requests/api/v1/recommendations_request_spec.rb
+++ spec/requests/api/v1/recommendations_request_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Recommendation, type: :request do
-  
+
   it "sends a list of recommendations for a single user" do
     create_list(:user, 10)
-    create_list(:recommendation, 5)
+    create_list(:recommendation, 3)
 
-    get '/api/v1/recommendations'
+    get '/api/v1/users/:user_id/recommendations'
 
     expect(response).to be_successful
   end
