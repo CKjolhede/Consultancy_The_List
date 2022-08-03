cf992850750d92f0c297bfffb4b4531499fe4132
Eric Espindola <eric_e_2012@hotmail.com>
2022-08-03 14:19:20 -0700

    update enum status to include completed and rejected.

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,13 +1,13 @@
 class Recommendation < ApplicationRecord
   belongs_to :user
 
-  validates_presence_of :title 
-  validates_presence_of :media_id 
+  validates_presence_of :title
+  validates_presence_of :media_id
   validates_presence_of :media_type
   validates_presence_of :user_id
   validates_presence_of :recommended_by_id
   
-  enum status: {pending: 0, accepted: 1}
+  enum status: {pending: 0, accepted: 1, completed: 2, rejected: 3}
   enum media_type: {movie: 0, book: 1}
 
 end
